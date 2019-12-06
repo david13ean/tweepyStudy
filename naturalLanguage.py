08b14c66929d1c9a645683a71630f123c6517dd1
David Bean <davidhb@ad.it.et.byu.edu>
2019-12-05 17:18:15 -0700

    some sentiment analysis added

--- naturalLanguage.py
+++ naturalLanguage.py
@@ -2,9 +2,35 @@ import pymongo
 import numpy as np 
 import pandas as pd 
 import re
-import nltk 
 import matplotlib.pyplot as plt
+from nltk.tokenize import TweetTokenizer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["twitter"]
 mycol = mydb["junk"]
+
+print ("\nReturn every document:")
+count = 1
+
+tt = TweetTokenizer()
+df = pd.DataFrame(list(mycol.find()))
+df['tokenized_tweet'] = df['text'].apply(tt.tokenize)
+df['quoted_tokenized_tweet'] = df['quoted_text'].apply(tt.tokenize)
+
+sid = SentimentIntensityAnalyzer()
+df['tweet_polarity'] = df['text'].apply(sid.polarity_scores)
+
+print(df.loc[0])
+# for col in df.columns:
+#     print(df[col])
+#     count = count - 1
+#     print(count)
+#     # if count <= 0:
+#     #     break
+
+# for doc in mycol.find():
+#     print (doc)
+#     count = count - 1
+#     if count <= 0:
+#         break
