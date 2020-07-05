9ed9b9f7ee796ff8fb3fbe378ec04f4059e40355
david13ean <david13ean@gmail.com>
2020-07-05 09:37:07 -0600

    fixed some regex pandas replacement statements

--- mongo2excel.py
+++ mongo2excel.py
@@ -40,7 +40,7 @@ def _connect_mongo(host, port, username, password, db):
     return conn[db]
 
 
-def read_mongo(count, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
+def read_mongo(count, limit, query={}, host='localhost', port=27017, username=None, password=None, no_id=True):
     """ Read from Mongo and Store into DataFrame """
 
     # Connect to MongoDB
@@ -49,7 +49,7 @@ def read_mongo(count, query={}, host='localhost', port=27017, username=None, pas
     dfMain = pd.DataFrame()
     dbList = ["coronavirus", "cvq2", "cv3-q", "cv4-q", "cv5-q"]
     for collection in dbList:
-        cursor = db[collection].find().limit(10)
+        cursor = db[collection].find().limit(limit)
 
         # Expand the cursor and construct the DataFrame
         df = pd.DataFrame(list(cursor))
@@ -61,19 +61,25 @@ def read_mongo(count, query={}, host='localhost', port=27017, username=None, pas
     return dfMain
 
 def col_formatter(df):
-    df['text'] = df['text'].str.replace(r'(RT @\w*:\s)', '') # This removes retweet evidence in preparation for duplicate removal
-    df['text'] = df['text'].str.replace(r'(\shttps?:\/\/t\.co\/\w*)', '') # This twitter links in preparation for duplicate removal
-    df['quoted_text'] = df['quoted_text'].str.replace(r'(RT @\w*:\s)', '')
-    df['quoted_text'] = df['quoted_text'].str.replace(r'(\shttps?:\/\/t\.co\/\w*)', '')
+    # This removes retweet evidence, twitter links, and whitespace in preparation for duplicate removal
+    df['text'] = df['text'].str.replace('\n', ' ', regex=True).replace('\r', '', regex=True).replace().replace('\t', ' ', regex=True).replace(r'(RT @\w*:\s)', '', regex=True).replace(r'(\shttps?:\/\/t\.co\/\w*)', '', regex=True)
+    df['quoted_text'] = df['quoted_text'].str.replace('\n', ' ', regex=True).replace('\r', '', regex=True).replace().replace('\t', ' ', regex=True).replace(r'(RT @\w*:\s)', '', regex=True).replace(r'(https?:\/\/t\.co\/\w*)', '', regex=True)
+    # removes multi whitespace
+    df['text'] = df['text'].str.strip()
+    df['quoted_text'] = df['quoted_text'].str.strip()
+    df = df.drop_duplicates(subset=['text', 'quoted_text'])
     return df
 
 if __name__ == '__main__':
-    count = int(sys.argv[1])            
+    count = int(sys.argv[1]) 
+    limit = int(sys.argv[3])            
     print(sys.argv)
-    df = read_mongo(count, {}, 'localhost', 27017)
+    df = read_mongo(count, limit)
     df = delete_unneeded_cols(df)
     df = col_formatter(df)
 
     with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
-        print(df)
+        print("the last things to be printed:\n\nLIST")
+        print(df['text'])
+        print(df['quoted_text'])
     df.to_excel(sys.argv[2])
